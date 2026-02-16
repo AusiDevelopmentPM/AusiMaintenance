a26e28966ed247df9b6fd0d2c90f78f4b46c13ba
EyNoah1171 <javasnippets.coding@gmail.com>
2026-02-16 07:00:30 +0100

    Create Loader.php

---
+++ src/ADPM/Maintenance/Loader.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @org AusiDevelopmentPM
+ * @author EyNoah1171
+ * @year 2026
+ * @license Dont steal my Code.
+ */
+
+namespace ADPM\Maintenance;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class Loader extends PluginBase {
+
+    use SingletonTrait;
+
+    public function onEnable(): void
+    {
+        self::setInstance($this);
+        
+    }
+
+}
